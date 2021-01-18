11610046f2a8cdf0e77845e45f30daab50e58edd
Taxxon <emil.kack@gmail.com>
2021-01-18 15:12:16 +0100

    init to program, not done with cmake

---
+++ cmake-build-debug/CMakeFiles/vulkan3DStudio.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vulkan3DStudio.dir/main.cpp.o"
+  "vulkan3DStudio"
+  "vulkan3DStudio.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vulkan3DStudio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/vulkan3DStudio.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for vulkan3DStudio.
+# This may be replaced when dependencies are built.
